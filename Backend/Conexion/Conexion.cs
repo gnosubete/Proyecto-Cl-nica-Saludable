cd7490c896050285d7dda0f9f4143a6f990f76a1
gnosubete <gustavol.valdes@gmail.com>
2025-12-02 22:13:00 -0300

    Add files via upload

---
+++ Backend/Conexion/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica_de_salud
+{
+    public class Conexion
+    {
+        private static string connectionString = "Server=localhost;Database=clinica_saludable;Uid=root;Pwd=;";
+
+        public static MySqlConnection ObtenerConexion()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
