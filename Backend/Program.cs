eca058005c1e2aae1a5cf9af57da114b914b5925
gnosubete <gustavol.valdes@gmail.com>
2025-12-02 22:11:01 -0300

    Add files via upload

---
+++ Backend/Program.cs
@@ -0,0 +1,19 @@
+using Clinica_de_salud.Vistas;
+
+namespace Clinica_de_salud
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FormLogin());
+        }
+    }
+}
